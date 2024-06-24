1d3aa4387c9562c4a47008f7c88f8c2a96975c87
user23052036 <user23052036@gmail.com>
2024-06-25 02:18:27 +0530

    added new files

---
+++ C_Language/Skills/BASIC/001/001-solution.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+	
+    char s[100];
+    fgets(s,100,stdin);
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    printf("Hello, World!\n");
+    printf("%s",s);  
+    return 0;
+}
